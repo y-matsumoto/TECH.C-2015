90bc37b6a37f85736dae9ae7772e8a2ff6271bc6
maythazinsoe <tzsakura@gmail.com>
2015-04-25 15:14:37 +0900

    UTF-8にしました。
    よろしくお願いします。

--- submit/Maythazinsoe/kyukyuhyo.java
+++ submit/Maythazinsoe/kyukyuhyo.java
@@ -3,7 +3,7 @@ package kyukyuhyou;
 class   kyukyuhyo {
     public  static  void  main(String[] args) {
     	int  a, b ;
-		System.out.println( " \t  ‹ã ‹ã ‚Ì•\‚ğì‚éB" +
+		System.out.println( " \t  ä¹ä¹è¡¨ã§ã™" +
 				" \n" );
 		for (b=1 ; b<= 9 ; b++)  {
 			for (a=1 ; a<= 9 ; a++) {

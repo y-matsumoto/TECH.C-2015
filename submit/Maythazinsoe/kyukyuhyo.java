41dafcc02de6e575f8bfe5ff93167323f4132ae3
maythazinsoe <tzsakura@gmail.com>
2015-04-25 14:28:33 +0900

    Answer

---
+++ submit/Maythazinsoe/kyukyuhyo.java
@@ -0,0 +1,17 @@
+package kyukyuhyou;
+
+class   kyukyuhyo {
+    public  static  void  main(String[] args) {
+    	int  a, b ;
+		System.out.println( " \t  ‹ã ‹ã ‚Ì•\‚ğì‚éB" +
+				" \n" );
+		for (b=1 ; b<= 9 ; b++)  {
+			for (a=1 ; a<= 9 ; a++) {
+
+				System.out.print("  "+ a+ "*"+ b+"= "+ a*b +"\t");
+
+			}
+			System.out.println() ;
+		}
+    }
+}

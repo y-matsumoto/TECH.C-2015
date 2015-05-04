fcb74a3ed87e780726695648d023a0ebe74469eb
sitri <wc348nka@gmail.com>
2015-05-05 03:23:28 +0900

    add answer for 04/15

---
+++ submit/kouta_suzuki/0415/MultiplicationTablePrint.java
@@ -0,0 +1,13 @@
+/**
+ * 九九表を表示する。
+ * @author sitri
+ */
+
+package MultiplicationTable;
+public class MultiplicationTablePrint {
+
+	public static void main(String[] args) {
+		MultiplicationTable StarndardTable = new MultiplicationTable(9, 9);
+		StarndardTable.print();
+	}
+}

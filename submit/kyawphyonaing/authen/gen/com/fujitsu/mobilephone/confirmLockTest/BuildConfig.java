9e72a2a5a1e98f1b8fadec605972c9753a43d288
kyaw phyo naing <iksan.kophyo@gmail.com>
2015-09-05 15:54:08 +0900

    auth

---
+++ submit/kyawphyonaing/authen/gen/com/fujitsu/mobilephone/confirmLockTest/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.fujitsu.mobilephone.confirmLockTest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
